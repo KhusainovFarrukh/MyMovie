662b5a82f305becb33ec192e7a4d9634f7c7d974
farrukh_kh <farrukhbekkhusainov@gmail.com>
2020-12-23 00:59:53 +0500

    Added additional files for homework

---
+++ app/src/main/java/academy/android/mymovie/data/Actor.kt
@@ -0,0 +1,7 @@
+package academy.android.mymovie.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ app/src/main/java/academy/android/mymovie/data/Genre.kt
@@ -0,0 +1,3 @@
+package academy.android.mymovie.data
+
+data class Genre(val id: Int, val name: String)
